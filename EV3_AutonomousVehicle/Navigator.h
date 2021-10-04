d18ff51b15b9c134f743d735ab0dff9f7ab7e1b8
Sathvik Chereddy <56492353+Sathware@users.noreply.github.com>
2021-10-04 15:57:47 -0400

    Fleshed out Navigator class

--- EV3_AutonomousVehicle/Navigator.h
+++ EV3_AutonomousVehicle/Navigator.h
@@ -8,9 +8,12 @@ private:
 	Vec toDestination;
 	Vec direction;
 	int degrees;// valid values are [0, 360)
+	void makeTerminal(int& invalidAngle);
 	static constexpr char speed = 10;
 public:
 	Navigator(Vec destination); //Robot must face directly towards destination
-	bool Turn(const int degreesIn); //Positive rotate left, negative rotate right relative to destination
+	bool Turn(const int degreesIn); //Positive rotate left, negative rotate right, must be between [0, 360)
 	bool FaceDestination();
+	bool Stop();
+	bool MoveToDestination();
 };
